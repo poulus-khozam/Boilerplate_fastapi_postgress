56dac3a24bb22ecb474a3752eb6a3a479944fc8f
poulus <118904375+poulus-khozam@users.noreply.github.com>
2025-10-20 03:31:47 +0300

    Update database.py

--- src/database.py
+++ src/database.py
@@ -2,7 +2,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from .core.config import settings
+from core.config import settings
 
 SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
 
