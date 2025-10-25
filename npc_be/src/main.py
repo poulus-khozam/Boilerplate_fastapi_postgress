b8868a9a3e6c50411167049ce85eaee9a973a4c1
poulus khozam <poulus.khozam@gmail.com>
2025-10-26 00:58:42 +0300

    src/main.py (with CORS enabled)

--- npc_be/src/main.py
+++ npc_be/src/main.py
@@ -1,5 +1,6 @@
 # src/main.py
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routes import auth, totp
 from database import Base, engine
 
@@ -8,6 +9,19 @@ from database import Base, engine
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:5173",  # The origin of your Vue.js front-end
+    "http://localhost",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods: GET, POST, etc.
+    allow_headers=["*"],  # Allows all headers
+)
 
 app.include_router(auth.router, tags=["Authentication"])
 app.include_router(totp.router, tags=["TOTP"])
