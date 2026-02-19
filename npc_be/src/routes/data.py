3229de2325e38e68e4a2b9b98ba3e767d51db6fd
poulus <118904375+poulus-khozam@users.noreply.github.com>
2026-02-19 13:09:29 +0200

    Add function to get distinct profile members

--- npc_be/src/routes/data.py
+++ npc_be/src/routes/data.py
@@ -4,6 +4,7 @@ from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 from database import get_db
 from models.ch_data import ChData
+from models.profile_details import ChProfileDetails
 from schemas.ch_data import ChDataResponse
 from schemas.token import Token 
 from schemas.password import ChangePassword 
@@ -18,6 +19,14 @@ from typing import List
 import json
 import os
 
+def get_distinct_profile_members(db: Session, location: int, profile_id: int, year: int, month: int):
+    return db.query(ChProfileDetails.id, ChData.name)\
+        .join(ChData, ChProfileDetails.id == ChData.id)\
+        .filter(ChProfileDetails.location == location)\
+        .filter(ChProfileDetails.profile_id == profile_id)\
+        .filter(ChProfileDetails.dyear == year)\
+        .filter(ChProfileDetails.dmonth == month)\
+        .distinct().all()
 
 # Define the MenuItem class so the route can use it as a response_model
 class MenuItem(BaseModel):
